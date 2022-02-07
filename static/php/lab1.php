6a5d9bf8de49c5e70a933cc7526c55c3e8ad7916
Dmitriy Trofimov <princessselestiapwnz@mail.ru>
2022-02-07 13:02:09 +0400

    Refactored a little bit

--- static/php/lab1.php
+++ static/php/lab1.php
@@ -2,9 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Моя 1 лаба</title>
+    <title>Actions on sets</title>
     <script type = "text/javascript" src = "../js/lab1.js"></script>
-    <!-- <script type = "text/javascript" src = "/scripts/DarkLight.js"></script> -->
 </head>
 <body>
 <h1>Л/р №1</h1>
@@ -20,25 +19,15 @@
             <td> Введите элементы 2 массива </td>
             <td> <input type = "text" id = "second_array" value = "" size = "150" /><br> </td>
         </tr>
-        <!-- <tr>
-            <td> Введите матрицу</td>
-            <td> <textarea id = "mass3" value = ""> Тут вводится матрица </textarea> <br> </td>
-        </tr> -->
         <tr><td colspan="2"> <input type="button" value = "Merge sets" onclick = "merge_sets();"> </td></tr>
         <tr><td colspan="2"> <input type="button" value = "Intersect sets" onclick = "intersect_sets();"> </td></tr>
         <tr><td colspan="2"> <input type="button" value = "Symmetric differece of 2 sets" onclick = "symmetric_difference();"> </td></tr>
         <tr><td colspan="2"> <input type="button" value = "Differece of A/B" onclick = "difference_a_on_b();"> </td></tr>
         <tr><td colspan="2"> <input type="button" value = "Differece of B/A" onclick = "difference_b_on_a();"> </td></tr>
-        <!-- <tr>
-            <td> <input type="button" value = "Смена темы" onclick = "ChangeTheme();"> </td>
-        </tr> -->
         <tr>
             <td colspan="2"> <p id="output"></p> </td>
         </tr>
     </table>
 </form>
-
-<div id = "outResult"> </div>
-
 </body>
 </html>
