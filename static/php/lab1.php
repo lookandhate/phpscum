2170eebe2210577ade754693989e26e3770c9e6d
Dmitriy Trofimov <princessselestiapwnz@mail.ru>
2022-02-09 22:02:36 +0400

    Added bootstrap

--- static/php/lab1.php
+++ static/php/lab1.php
@@ -6,6 +6,7 @@
     <script type="text/javascript" src="../js/lab1.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="../styles/lab1.css" rel="stylesheet">
 
 </head>
 <body>
@@ -15,21 +16,23 @@
 <form method="" action="">
     <div class="form-group">
         <label for="first_array">First set</label>
-        <input type="text" class="form-control" id="first_array" placeholder="Input first set" size="30%"/><br>
+        <input type="text" class="form-control input-field" id="first_array" placeholder="Input first set"
+               size="30%"/><br>
     </div>
 
     <div class="form-group">
         <label for="second_array">Second set</label>
-        <input type="text" class="form-control" id="second_array" placeholder="Input second set"/><br>
+        <input type="text" class="form-control input-field" id="second_array" placeholder="Input second set"/><br>
     </div>
 
-    <input type="button" class="btn btn-primary" value="Merge sets" onclick="merge_sets();">
-    <input type="button" class="btn btn-primary" value="Intersect sets" onclick="intersect_sets();">
-    <input type="button" class="btn btn-primary" value="Symmetric difference of 2 sets"
-           onclick="symmetric_difference();">
-    <input type="button" class="btn btn-primary" value="Difference of A/B" onclick="difference_a_on_b();">
-    <input type="button" class="btn btn-primary" value="Difference of B/A" onclick="difference_b_on_a();">
-
+    <div class="btn-group-vertical">
+        <input type="button" class="btn btn-primary button-style" value="Merge sets" onclick="merge_sets();">
+        <input type="button" class="btn btn-primary button-style" value="Intersect sets" onclick="intersect_sets();">
+        <input type="button" class="btn btn-primary button-style" value="Symmetric difference of 2 sets"
+               onclick="symmetric_difference();">
+        <input type="button" class="btn btn-primary button-style" value="Difference of A/B" onclick="difference_a_on_b();">
+        <input type="button" class="btn btn-primary button-style" value="Difference of B/A" onclick="difference_b_on_a();">
+    </div>
 </form>
 </body>
 </html>
